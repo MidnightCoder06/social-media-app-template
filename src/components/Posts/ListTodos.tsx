ccadbeca40498d9a56c54ce1dde1cbe3a563787d
Jean Emile Leconte II <jeanemileleconteii@Jeans-MacBook-Air.local>
2022-01-13 21:31:04 -0600

    added loading and error states

--- src/components/Posts/ListTodos.tsx
+++ src/components/Posts/ListTodos.tsx
@@ -2,19 +2,27 @@ import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Row from './Row';
 
+// TODO: add infinite scroll to a certain point + paganation like a tabbed wizard
+
 const ListTodos = () => {
 
   const [todos, setTodos] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   const getTodos = async () => {
     try {
+      setIsLoading(true);
+      setHasError(false);
       // by default a get request
       const response = await fetch('http://localhost:5000/todos');
       const jsonData = await response.json();
       setTodos(jsonData)
     } catch (err) {
       console.error(err)
+      setHasError(true);
     }
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -23,14 +31,25 @@ const ListTodos = () => {
 
   return (
     <>
-      <div className='thead'>
-        <Header />
-      </div>
-      <div className='tbody'>
-        {todos.map(todo => (
-          <Row key={todo.todo_id} todo={todo} todos={todos} setTodos={setTodos} />
-        ))}
-      </div>
+    {hasError && <p>Something went wrong.</p>}
+    {
+      isLoading ? (
+        <p> Loading ...</p>
+      ) :
+        (
+          <>
+            <div className='thead'>
+              <Header />
+            </div>
+            <div className='tbody'>
+              {todos.map(todo => (
+                <Row key={todo.todo_id} todo={todo} todos={todos} setTodos={setTodos} />
+              ))}
+            </div>
+          </>
+        )
+    }
+      
     </>
   );
 }
